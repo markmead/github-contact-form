5e753329471ded58a35b8a85be591e087525e408
markmead <itsmarkmead@gmail.com>
2022-03-30 17:05:44 +0100

    Add error handling

--- src/index.js
+++ src/index.js
@@ -3,14 +3,31 @@ export default class {
     this.octokit = octokit;
   }
 
-  sendMessage(message) {
+  async sendMessage(message) {
     const { owner, repo, title, body } = message;
 
-    this.octokit.request("POST /repos/{owner}/{repo}/issues", {
-      owner,
-      repo,
-      title,
-      body,
-    });
+    if (!owner || !repo || !title) {
+      return {
+        status: 422,
+        message: "Missing required parameters",
+        errors: {
+          owner: !owner,
+          repo: !repo,
+          title: !title,
+        },
+      };
+    }
+
+    const response = await this.octokit.request(
+      "POST /repos/{owner}/{repo}/issues",
+      {
+        owner,
+        repo,
+        title,
+        body,
+      }
+    );
+
+    return response;
   }
 }

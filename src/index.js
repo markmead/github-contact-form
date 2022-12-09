445b6f5da248dd0e8c28d79c5275b29294026edc
Mark Mead <itsmarkmead@gmail.com>
2022-12-09 08:49:08 +0000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
 export default class {
   constructor(octokit) {
-    this.octokit = octokit;
+    this.octokit = octokit
   }
 
   async sendMessage(message) {
-    const { owner, repo, title, body, spam } = message;
+    const { owner, repo, title, body, spam } = message
 
     if (!owner || !repo || !title) {
       return {
         status: 422,
-        message: "Missing required parameters",
+        message: 'Missing required parameters',
         errors: {
           owner: !owner,
           repo: !repo,
           title: !title,
         },
-      };
+      }
     }
 
     if (spam) {
       return {
         status: 422,
-        message: "Spam detected",
-      };
+        message: 'Spam detected',
+      }
     }
 
     const response = await this.octokit.request(
-      "POST /repos/{owner}/{repo}/issues",
+      'POST /repos/{owner}/{repo}/issues',
       {
         owner,
         repo,
         title,
         body,
       }
-    );
+    )
 
-    return response;
+    return response
   }
 }

a2fb7ff56cf8af823aa3f6e77571cb6aa1f943ed
Mark Mead <50486078+markmead@users.noreply.github.com>
2022-12-14 14:21:52 +0000

    Update - ES Build 0.16 (#5)

--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,14 @@
-build({
-  entryPoints: [`src/index.js`],
-  outfile: `dist/index.js`,
-  target: ["node10.4"],
-  platform: "node",
-});
+buildPlugin({
+  entryPoints: ['src/index.js'],
+  outfile: 'dist/index.js',
+  platform: 'neutral',
+  mainFields: ['main', 'module'],
+})
 
-function build(options) {
-  options.define || (options.define = {});
-
-  return require("esbuild")
-    .build({ ...options, minify: true, bundle: true })
-    .catch(() => process.exit(1));
+function buildPlugin(buildOptions) {
+  return require('esbuild').buildSync({
+    ...buildOptions,
+    minify: true,
+    bundle: true,
+  })
 }
